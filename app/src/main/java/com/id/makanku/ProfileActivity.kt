defb4c209ba1de9cbfb7c4665cf74b311a854fb2
Achdan37 <achdanazizi@gmail.com>
2025-11-23 14:13:27 +0700

    kiwkiw

--- app/src/main/java/com/id/makanku/ProfileActivity.kt
+++ app/src/main/java/com/id/makanku/ProfileActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,4 +18,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
