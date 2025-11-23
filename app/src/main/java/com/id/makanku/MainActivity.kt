ee5f73401ab2c2d3c9179567a7ceacb01d15c4e2
Achdan37 <achdanazizi@gmail.com>
2025-11-23 15:13:34 +0700

    ini ach

--- app/src/main/java/com/id/makanku/MainActivity.kt
+++ app/src/main/java/com/id/makanku/MainActivity.kt
@@ -20,5 +20,6 @@ class MainActivity : AppCompatActivity() {
         Log.d("TEST_KOLAB", "Halo dari Ridwan afriza!")
         Log.d("TEST_KOLAB", "Halo dari star!")
         Log.d("TEST_KOLAB", "Halo dari tar!")
+        Log.d("TEST_KOLAB", "Halo dari ach!")
     }
 }
