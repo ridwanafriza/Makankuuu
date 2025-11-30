7c53901aa440820b5658e6d098b9ffc612a77ac0
starinatiara <starinapramesti@gmail.com>
2025-11-30 18:49:48 +0700

    ButtonRegisterUMKM_tarin

--- app/src/main/java/com/id/makanku/LoginRegister/RegisterActivity.kt
+++ app/src/main/java/com/id/makanku/LoginRegister/RegisterActivity.kt
@@ -15,19 +15,54 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+        // Hindari ketiban status bar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Tombol Register & Login
         val btnRegister = findViewById<Button>(R.id.button)
         val txtLogin = findViewById<TextView>(R.id.textView5)
 
+        // SEGMENT tombol Mahasiswa & UMKM
+        val btnMahasiswa = findViewById<TextView>(R.id.btnMahasiswa)
+        val btnUmkm = findViewById<TextView>(R.id.btnUmkm)
+
+        // Default Mahasiswa aktif
+        btnMahasiswa.setBackgroundResource(R.drawable.bg_segment_selected)
+        btnMahasiswa.setTextColor(resources.getColor(android.R.color.black))
+        btnUmkm.setBackgroundResource(android.R.color.transparent)
+        btnUmkm.setTextColor(resources.getColor(android.R.color.darker_gray))
+
+        // Klik Mahasiswa → jadi aktif
+        btnMahasiswa.setOnClickListener {
+            btnMahasiswa.setBackgroundResource(R.drawable.bg_segment_selected)
+            btnMahasiswa.setTextColor(resources.getColor(android.R.color.black))
+
+            btnUmkm.setBackgroundResource(android.R.color.transparent)
+            btnUmkm.setTextColor(resources.getColor(android.R.color.darker_gray))
+        }
+
+        // Klik UMKM → jadi aktif
+        btnUmkm.setOnClickListener {
+            btnUmkm.setBackgroundResource(R.drawable.bg_segment_selected)
+            btnUmkm.setTextColor(resources.getColor(android.R.color.black))
+
+            btnMahasiswa.setBackgroundResource(android.R.color.transparent)
+            btnMahasiswa.setTextColor(resources.getColor(android.R.color.darker_gray))
+        }
+
+        // Tombol Register → pindah ke Login
         btnRegister.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
+
+        // Text "Login" → pindah ke Login
         txtLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
