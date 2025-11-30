612d41537088c735e8c441a8bcc96e87eff409fb
starinatiara <starinapramesti@gmail.com>
2025-11-30 12:06:07 +0700

    RegisterkeLogin_tarin

--- app/src/main/java/com/id/makanku/LoginRegister/RegisterActivity.kt
+++ app/src/main/java/com/id/makanku/LoginRegister/RegisterActivity.kt
@@ -1,6 +1,9 @@
 package com.id.makanku.LoginRegister
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +19,19 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnRegister = findViewById<Button>(R.id.button)
+        val txtLogin = findViewById<TextView>(R.id.textView5)
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        txtLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
