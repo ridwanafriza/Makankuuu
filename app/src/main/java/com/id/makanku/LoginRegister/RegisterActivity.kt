1573cd6280b88cb403e32aab0a338386e9f0e6c5
starinatiara <starinapramesti@gmail.com>
2025-12-02 14:22:20 +0700

    Buttoniconeyeregister_tarin

--- app/src/main/java/com/id/makanku/LoginRegister/RegisterActivity.kt
+++ app/src/main/java/com/id/makanku/LoginRegister/RegisterActivity.kt
@@ -2,7 +2,10 @@ package com.id.makanku.LoginRegister
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,12 +14,16 @@ import androidx.core.view.WindowInsetsCompat
 import com.id.makanku.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var isPasswordVisible = false
+    private var isConfirmPasswordVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
 
-        // Hindari ketiban status bar
+        // Menghindari layout ketiban status bar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -27,17 +34,17 @@ class RegisterActivity : AppCompatActivity() {
         val btnRegister = findViewById<Button>(R.id.button)
         val txtLogin = findViewById<TextView>(R.id.textView5)
 
-        // SEGMENT tombol Mahasiswa & UMKM
+        // SEGMENT Mahasiswa & UMKM
         val btnMahasiswa = findViewById<TextView>(R.id.btnMahasiswa)
         val btnUmkm = findViewById<TextView>(R.id.btnUmkm)
 
-        // Default Mahasiswa aktif
+        // Default: Mahasiswa aktif
         btnMahasiswa.setBackgroundResource(R.drawable.bg_segment_selected)
         btnMahasiswa.setTextColor(resources.getColor(android.R.color.black))
         btnUmkm.setBackgroundResource(android.R.color.transparent)
         btnUmkm.setTextColor(resources.getColor(android.R.color.darker_gray))
 
-        // Klik Mahasiswa → jadi aktif
+        // Klik Mahasiswa → aktif
         btnMahasiswa.setOnClickListener {
             btnMahasiswa.setBackgroundResource(R.drawable.bg_segment_selected)
             btnMahasiswa.setTextColor(resources.getColor(android.R.color.black))
@@ -46,7 +53,7 @@ class RegisterActivity : AppCompatActivity() {
             btnUmkm.setTextColor(resources.getColor(android.R.color.darker_gray))
         }
 
-        // Klik UMKM → jadi aktif
+        // Klik UMKM → aktif
         btnUmkm.setOnClickListener {
             btnUmkm.setBackgroundResource(R.drawable.bg_segment_selected)
             btnUmkm.setTextColor(resources.getColor(android.R.color.black))
@@ -54,6 +61,20 @@ class RegisterActivity : AppCompatActivity() {
             btnMahasiswa.setBackgroundResource(android.R.color.transparent)
             btnMahasiswa.setTextColor(resources.getColor(android.R.color.darker_gray))
         }
+
+        val edtPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val edtKonfirmasi = findViewById<EditText>(R.id.konfirmasipassword)
+        val eyePassword = findViewById<ImageView>(R.id.eyePassword)
+        val eyeConfirm = findViewById<ImageView>(R.id.eyeConfirmPassword)
+
+        eyePassword.setOnClickListener {
+            isPasswordVisible = togglePassword(edtPassword, eyePassword, isPasswordVisible)
+        }
+
+        eyeConfirm.setOnClickListener {
+            isConfirmPasswordVisible = togglePassword(edtKonfirmasi, eyeConfirm, isConfirmPasswordVisible)
+        }
+
 
         // Tombol Register → pindah ke Login
         btnRegister.setOnClickListener {
@@ -68,5 +89,19 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+    private fun togglePassword(edit: EditText, eye: ImageView, isVisible: Boolean): Boolean {
+        val newVisible = !isVisible
+
+        if (newVisible) {
+            edit.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            eye.setImageResource(R.drawable.baseline_visibility_24)
+        } else {
+            edit.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            eye.setImageResource(R.drawable.baseline_visibility_off_24)
+        }
+
+        edit.setSelection(edit.text.length) // supaya kursor tidak pindah
+        return newVisible
     }
 }
