c65fdcb81f2424ebc1130d3d0769671ab8d875bc
ridwan <ralistiyono09@gmail.com>
2025-12-03 11:41:25 +0700

    membuat navbar home, discount, history, chat, minus tulisan belum muncul -ridwan

--- app/src/main/java/com/id/makanku/MainActivity.kt
+++ app/src/main/java/com/id/makanku/MainActivity.kt
@@ -1,35 +1,23 @@
 package com.id.makanku
 
-import android.os.Bundle
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.ui.setupWithNavController
+import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.navigation.findNavController
-
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        }
-        val navController = findNavController(R.id.nav_host_fragment)
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navController = navHostFragment.navController
 
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNav.setupWithNavController(navController)
-
     }
 }
-
-
-
