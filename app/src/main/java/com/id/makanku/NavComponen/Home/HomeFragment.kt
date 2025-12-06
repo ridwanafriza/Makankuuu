3a98ac7e18b9cb86d81764d3b1d4b9572e8c4362
Achdan37 <achdanazizi@gmail.com>
2025-12-06 11:58:59 +0700

    MEMPERBAIKI  lagi menu search -achdan

--- app/src/main/java/com/id/makanku/NavComponen/Home/HomeFragment.kt
+++ app/src/main/java/com/id/makanku/NavComponen/Home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.id.makanku.NavComponen.Home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.id.makanku.R
 import com.id.makanku.databinding.FragmentHomeBinding
+import com.id.makanku.SearchRestorant.SearchRestorant
+import kotlin.jvm.java
 
 class HomeFragment : Fragment() {
 
@@ -24,6 +27,16 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // ====== PINDAH KE SEARCH =======
+        binding.etSearch.setOnClickListener {
+            startActivity(Intent(requireContext(), SearchRestorant::class.java))
+        }
+
+        binding.layoutSearch.setOnClickListener {
+            startActivity(Intent(requireContext(), SearchRestorant::class.java))
+        }
+        // =================================
 
         val listUmkm = listOf(
             UmkmModel(1, "warung sambal kuy", "jl. serayu timur v", "1.5 km", "5–10 min", 50, R.drawable.ayam_bakar),
